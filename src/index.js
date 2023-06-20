0abe4def590f162940c68e1f70830453ed40376c
emdoem <125324546+emdoem@users.noreply.github.com>
2023-06-20 23:48:43 +0200

    add external database with tasks

--- src/index.js
+++ src/index.js
@@ -2,14 +2,39 @@ const taskForm = document.querySelector('form');
 const taskInput = document.querySelector('input');
 const taskList = document.querySelector('ul');
 
-let tasks = [];
+const BASE_URL = "http://localhost:5000/tasks/";
+const TasksAPI = {
+  getAllTasks: async function() {
+    const response = await makeRequest(BASE_URL, "GET");
+    const tasks = await response.json();
+    return tasks;
+  }
+}
 
+async function makeRequest(url, method, body) {
+  const jsonBody = body ? JSON.stringify(body) : undefined;
+  const response = await window.fetch(url, {
+          method: method,
+          headers: {
+              "Content-Type": "application/json"
+          },
+          body: jsonBody
+      });
+  if (!response.ok) {
+      throw new Error("Sumting wong!");
+  }
+  return response;
+}
+
+let tasks;
+ 
 function renderTasks() {
   taskList.innerHTML = '';
-  for (let i = 0; i < tasks.length; i++) {
+    for (let i = 0; i < tasks.length; i++) {
     // const task = tasks[i]; - replace a variable with an HTML node for styling purposes
     const task = document.createElement('a');
-    task.textContent = tasks[i];
+    task.textContent = tasks[i].title;
+    console.log(tasks);
     const li = document.createElement('li');
     li.className = 'task';
     // 
@@ -43,11 +68,18 @@ function newRenderTasks() {
 
 function addTask(event) {
   event.preventDefault();
-  tasks.push(taskInput.value);
+  tasks.push({
+    "title": taskInput.value,
+    "checked": false
+  });
   taskInput.value = '';
   renderTasks();
 }
 
 taskForm.addEventListener('submit', addTask); 
 
-renderTasks();
+TasksAPI.getAllTasks()
+  .then((tasksFromAPI) => {
+    tasks = tasksFromAPI;
+    renderTasks();
+  });
